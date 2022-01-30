1dc9bc0a980792a84fef9bf8e168003b45f412af
Umme Salma <ummes3527@gmail.com>
2022-01-30 23:37:40 +0600

    nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import './Navbar.css';
+import "./Navbar.css";
+import images from "../../constants/images";
+import { GiCastle } from "react-icons/gi";
 
-const Navbar = () => (
-  <div>
-    Navbar
-  </div>
-);
-
+const Navbar = () => {
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.gericht} alt="app-logo" />
+      </div>
+      <ul>
+        <li className="app__navbar-links">
+          <a href=""></a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 export default Navbar;
