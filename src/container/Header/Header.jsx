28490431f52aec86c53baf46f38e528d82055297
Umme Salma <ummes3527@gmail.com>
2022-02-02 15:28:24 +0600

    header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -2,13 +2,24 @@ import React from "react";
 
 import "./Header.css";
 import { SubHeading } from "../../components";
+import { images } from "../../constants";
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id home>
     <div className="app__wrapper_info">
       <SubHeading title="Chase the new flavour"></SubHeading>
       <h1 className="app__header-h1">The key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat
+        morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet
+        tellus
+      </p>
+      <button type="button" className="custom__button">
+        Explore Menu
+      </button>
     </div>
-    <div className="app__wrapper_img"></div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="welcome-img" />
+    </div>
   </div>
 );
 
