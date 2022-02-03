01e3a005fd81b9be3f3a4209667df30fbb2da8fb
Umme Salma <ummes3527@gmail.com>
2022-02-04 00:36:40 +0600

    special menu

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -10,10 +10,16 @@ const MenuItem = ({ title, price, tags }) => (
           {title}
         </p>
       </div>
+      <div className="app__menuitem-dash"></div>
 
       <div className="app__menuitem-price">
         <p className="p__cormorant">{price}</p>
       </div>
+    </div>
+    <div className="app__menuitem-sub">
+      <p className="p__opensana" style={{ color: "#AAA" }}>
+        {tags}
+      </p>
     </div>
   </div>
 );
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -28,7 +28,7 @@ const SpecialMenu = () => (
         <img src={images.menu} alt="" />
       </div>
       <div className="app__specialMenu-menu_cocktails flex__center">
-        <p className="app__specialMenu_menu_heading">Wine & Beer</p>
+        <p className="app__specialMenu_menu_heading">Cocktails</p>
         <div className="app__specialMenu_menu_items">
           {data.cocktails.map((cocktail, index) => (
             <MenuItem
