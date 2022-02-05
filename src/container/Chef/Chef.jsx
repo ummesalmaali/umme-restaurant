4dc4afe11485328291370fb71969a97cb379a1f2
Umme Salma <ummes3527@gmail.com>
2022-02-05 21:38:38 +0600

    chef-done

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -12,6 +12,24 @@ const Chef = () => (
     <div className="app__wrapper_info">
       <SubHeading title="Chef's Word" />
       <h1 className="headtext__cormorant">What We Believe in</h1>
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote-img" />
+          <p className="p__opensans">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          </p>
+        </div>
+        <p className="p__opensans">
+          Nam impedit iure excepturi libero illo atque, reprehenderit delectus
+          minus laborum quia molestiae quis error! Deserunt eos libero adipisci
+          saepe, repellendus motivated me always think repellat.
+        </p>
+        <div className="app__chef-sign">
+          <p>Kevin Luo</p>
+          <p className="p__opensans">Chef & Founder</p>
+          <img src={images.sign} alt="" />
+        </div>
+      </div>
     </div>
   </div>
 );
