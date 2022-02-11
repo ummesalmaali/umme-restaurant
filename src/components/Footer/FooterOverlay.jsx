41c43260f9c191771fb6a1924cff185311e3045a
Umme Salma <ummes3527@gmail.com>
2022-02-11 21:56:43 +0600

    newsletter

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import './FooterOverlay.css';
+import "./FooterOverlay.css";
 
 const FooterOverlay = () => (
-  <div>
-    FooterOverlay
+  <div className="app__footerOverlay">
+    <div className="app__footOverlay-black" />
+    <div className="app__footerOverlay-img app__"></div>
   </div>
 );
 
