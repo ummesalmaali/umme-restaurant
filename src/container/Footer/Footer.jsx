7a43f6f07e5b64db6492a935458dde97715204fc
Umme Salma <ummes3527@gmail.com>
2022-02-14 22:03:42 +0600

    footer-section

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -8,7 +8,22 @@ const Footer = () => (
   <div className="app__footer section__padding">
     <FooterOverlay />
     <Newsletter />
-    <div className="app__footer-links"></div>
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">
+          Chandgaon Residential Area, Chandgaon, Block-B, Road No: 14,
+          Chittagong
+        </p>
+        <p className="p__opensans">+880 1784124267</p>
+        <p className="p__opensans">+880 1780181577</p>
+      </div>
+      <div className="app__footer-links_logo"></div>
+      <div className="app__footer-links_work"></div>
+    </div>
+    <div className="footer__copyright">
+      <p className="p__opensans"> 2022 Umme Salma Ali. All Rights Reserved</p>
+    </div>
   </div>
 );
 
