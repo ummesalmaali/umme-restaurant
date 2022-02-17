2892eb7b6b9f7902c485803a28b7988a2d993fba
Umme Salma <ummes3527@gmail.com>
2022-02-17 21:09:57 +0600

    footer-info

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -3,6 +3,7 @@ import { FooterOverlay, Newsletter } from "../../components";
 import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
 
 import "./Footer.css";
+import { images } from "../../constants";
 
 const Footer = () => (
   <div className="app__footer section__padding">
@@ -18,8 +19,28 @@ const Footer = () => (
         <p className="p__opensans">+880 1784124267</p>
         <p className="p__opensans">+880 1780181577</p>
       </div>
-      <div className="app__footer-links_logo"></div>
-      <div className="app__footer-links_work"></div>
+      <div className="app__footer-links_logo">
+        <img src={images.gericht} alt="footer-logo" />
+        <p className="p__opensans">"The best way to find your meal"</p>
+        <img
+          src={images.spoon}
+          alt="spoon"
+          className="spoon__img"
+          style={{ marginTop: "15px" }}
+        />
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
+      </div>
+      <div className="app__footer-links_work">
+        <h1 className="app__footer-headtext">Working Hours</h1>
+        <p className="p__opensans">Monday- Friday:</p>
+        <p className="p__opensans"> 11:00 AM - 10:00 PM</p>
+        <p className="p__opensans">Saturday - Sunday: 11:00 AM to 10:00 PM</p>
+        <p className="p__opensans"> 11:00 AM to 10:00 PM</p>
+      </div>
     </div>
     <div className="footer__copyright">
       <p className="p__opensans"> 2022 Umme Salma Ali. All Rights Reserved</p>
